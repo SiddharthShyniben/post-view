e1a86377a706132bb032651f6fbb0ad764fddf14
Siddharth <siddharth.muscat@gmail.com>
2021-08-30 14:25:09 +0530

    Restart after broken git history

---
+++ _javascript/view.js
@@ -0,0 +1,13 @@
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+
+if (params.title) document.querySelector('#post-title').innerHTML = `${params.link ? `<a href='${params.link}'>` : ''}${params.title}${params.link ? '</a>' : ''}`;
+if (params.twitter) document.querySelector('#author-twitter').innerHTML = `<a href='https://twitter.com/${params.twitter.substring(1)}'>${params.twitter}</a>`
+if (params.description) document.querySelector('#description').innerHTML = params.description;
+if (params.cover) {
+	let img = document.querySelector('#image');
+	img.setAttribute('src', params.cover);
+	img.setAttribute('alt', params.title || '')
+} else {
+	document.querySelector('.card-image').remove();
+}
